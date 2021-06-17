545b5e92a9e9739160b8654761b41a84183b4bae
Anja Tomić <anja.tomic099@gmail.com>
2021-06-17 20:34:55 +0200

    uncomment code for swagger

--- Blog.Api/Startup.cs
+++ Blog.Api/Startup.cs
@@ -73,39 +73,39 @@ namespace Blog.Api
             //services.AddAutoMapper(typeof(QueryableExtensions).Assembly);
 
             //SWAGGER
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog Project", Version = "v1" });
-            //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            //    {
-            //        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-            //          Enter 'Bearer' [space] and then your token in the text input below.
-            //          \r\n\r\nExample: 'Bearer 12345abcdef'",
-            //        Name = "Authorization",
-            //        In = ParameterLocation.Header,
-            //        Type = SecuritySchemeType.ApiKey,
-            //        Scheme = "Bearer"
-            //    });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blog Project", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
 
-            //    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-            //    {
-            //        {
-            //            new OpenApiSecurityScheme
-            //            {
-            //                Reference = new OpenApiReference
-            //                  {
-            //                    Type = ReferenceType.SecurityScheme,
-            //                    Id = "Bearer"
-            //                  },
-            //                  Scheme = "oauth2",
-            //                  Name = "Bearer",
-            //                  In = ParameterLocation.Header,
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                              {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                              },
+                              Scheme = "oauth2",
+                              Name = "Bearer",
+                              In = ParameterLocation.Header,
 
-            //            },
-            //            new List<string>()
-            //        }
-            //    });
-            //});
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
         }
 
@@ -118,12 +118,12 @@ namespace Blog.Api
             }
 
             //SWAGGER
-            //app.UseSwagger();
+            app.UseSwagger();
 
-            //app.UseSwaggerUI(c =>
-            //{
-            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger");
-            //});
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger");
+            });
 
             app.UseRouting();
             app.UseStaticFiles();
